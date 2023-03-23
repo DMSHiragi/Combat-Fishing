587fae6133f9a1d10aaf7ad1a4dfd02caa0e59f2
DMSHiragi <103024983+DMSHiragi@users.noreply.github.com>
2023-03-23 02:47:23 -0700

    Added object glow differences between object layers

---
+++ Open XR Test/Assets/Scripts/GlowRaycast.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
+public class GlowRaycast : MonoBehaviour
+{
+
+    public Transform cam;
+    public float hitDistance;
+    private RaycastHit hit;
+
+    public LayerMask grappleable;
+    public LayerMask swingable;
+    public LayerMask breakable;
+
+    private Outline gc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(cam.position, cam.forward, out hit, hitDistance, grappleable)){
+            gc = hit.collider.gameObject.GetComponent<Outline>();
+        
+            gc.enabled  = true;
+        }
+
+        else if(Physics.Raycast(cam.position, cam.forward, out hit, hitDistance, swingable)){
+            gc = hit.collider.gameObject.GetComponent<Outline>();
+        
+            gc.enabled  = true;
+        }
+
+        else if(Physics.Raycast(cam.position, cam.forward, out hit, hitDistance, breakable)){
+            gc = hit.collider.gameObject.GetComponent<Outline>();
+        
+            gc.enabled  = true;
+        }
+
+        else if(gc != null){
+            gc.enabled = false;
+        }
+    }
+}
