9dfa32a57cef8566d298d842c70e6ff4a4147d7f
Xinnoh <30382015+Xinnoh@users.noreply.github.com>
2023-04-03 07:39:47 -0700

    Complete Beach Scene
    Adds whale eating player, small fish tracker
    Settings are still set to debug mode (eg catch 1 fish to set off whale)

---
+++ Open XR Test/Assets/Scripts/FishingTracker.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.XR;
+using System;
+
+public class FishingTracker : MonoBehaviour
+{
+
+    private bool newFish;
+    private bool fishColour;
+    private float fishSize;
+    public TextMeshProUGUI textComponent;    
+
+    public void getFish(float Size, bool red){
+        string colour;
+        if(red){
+            colour = "Blue Fin - ";
+        }else{
+            colour = "Red Emperor - ";
+        }
+        float roundedFloat = (float) Math.Round(Size, 2);
+
+        textComponent.text += "\n" + colour + roundedFloat.ToString() + "m";
+    }
+
+
+    
+}
